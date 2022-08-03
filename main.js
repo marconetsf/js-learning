f71e72d80d4aa78b9847c4c3653ce26ce0230c71
Marcone Tenorio <marconetsf@gmail.com>
2022-08-03 11:29:07 -0300

    aula 5

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // pré processado
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSom(elementID) {
+    document.querySelector(elementID).play()
+}
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) 
+{
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 }
